f73aa421bc118d42b18df7278cf621a9bddb5bb5
ibrown <ibrown@twitter.com>
2018-12-13 10:38:06 -0800

    offset with overflow working

--- cabot.js
+++ cabot.js
@@ -1,19 +1,41 @@
 
 function applyRule(left_parent, middle_parent, right_parent) {
-console.log(left_parent + " " +  middle_parent + " " + right_parent);
+    console.log(left_parent + " " +  middle_parent + " " + right_parent);
     var input = parseInt((left_parent ? "1" : "0") +  (middle_parent ? "1" : "0") +  (right_parent ? "1" : "0"), 2);
-//switch(input) {
-  //     case 01011010
-console.log(input);
+    //switch(input) {
+    //     case 01011010
+    console.log(input);
 }
 
 function getColumnNumber(i) {
- console.log("POS: " + (i % (ctxt.canvas.width * 4)) / 4);
+    return  (i % (ctxt.canvas.width * 4)) / 4;
+}
+
+
+function getLeftParent(i, data) { // wrap to last element of parent line if at start of row
+    if (getColumnNumber(i) == 0) {
+	return data[i - 4] != 255;
+    } else {
+	return data[i - ((ctxt.canvas.width * 4) + 4)] != 255;
+    }
+}
+
+function getMiddleParent(i, data) {
+    return data[i - (ctxt.canvas.width * 4)] != 255;
+}
+
+function getRightParent(i, data) { // wrap to last element of parent line if at start of row
+    if (getColumnNumber(i) == ctxt.canvas.width - 1) {
+	return data[i - ((ctxt.canvas.width * 4) + (getColumnNumber(i) * 4))] != 255;
+    } else {
+	return data[i - ((ctxt.canvas.width * 4) - 4)] != 255;
+    }
+}
 
 var canvas = document.getElementById("foo");
 var ctxt =  canvas.getContext("2d");
 
-var imageData = ctxt.getImageData(0, 0, ctxt.canvas.height, ctxt.canvas.width)
+var imageData = ctxt.getImageData(0, 0, ctxt.canvas.height, ctxt.canvas.width);
 console.log("width:" + ctxt.canvas.width);
 console.log("height:" + ctxt.canvas.height);
 console.log(imageData.data);
@@ -23,11 +45,11 @@ for (var i=0; i < data.length; i += 4) {
 		  if (i < ctxt.canvas.width * 4 -1) {
 			  fill = Math.floor(Math.random() * 2)  == 1;
 		  } else {
-			  console.log("POS: " + (i % (ctxt.canvas.width * 4)) / 4);
+		      console.log("POS: " + getColumnNumber(i));
 			  
-			  var left_parent = data[i - ((ctxt.canvas.width * 4) + 4)] != 255;
-			  var middle_parent = data[i - (ctxt.canvas.width * 4)] != 255;
-			  var right_parent = data[i - ((ctxt.canvas.width * 4) - 4)] != 255;
+		      var left_parent = getLeftParent(i, data);
+		      var middle_parent = getMiddleParent(i, data);
+		      var right_parent = getRightParent(i, data);
 			  fill = applyRule(left_parent, middle_parent, right_parent);
 			 		  }
 
@@ -37,5 +59,5 @@ for (var i=0; i < data.length; i += 4) {
 		  data[i + 3] = 255;  // A value
 }
 
- imageData = new ImageData(data, ctxt.canvas.width, ctxt.canvas.height);
+imageData = new ImageData(data, ctxt.canvas.width, ctxt.canvas.height);
 ctxt.putImageData(imageData, 0, 0);
