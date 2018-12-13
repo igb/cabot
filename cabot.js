fc7ed074f3f7225f1c77bcfd60f4fd6d5ebc02fc
ibrown <ibrown@twitter.com>
2018-12-13 13:45:29 -0800

    nodifying js

--- cabot.js
+++ cabot.js
@@ -1,9 +1,12 @@
+var fs = require("fs");
+var Canvas = require("canvas");
 
-function applyRule(left_parent, middle_parent, right_parent) {
-    console.log(left_parent + " " +  middle_parent + " " + right_parent);
-    console.log((left_parent ? "1" : "0") +  (middle_parent ? "1" : "0") +  (right_parent ? "1" : "0"));
+
+function applyRule(left_parent, middle_parent, right_parent, rule_number) {
+
     var input = parseInt((left_parent ? "1" : "0") +  (middle_parent ? "1" : "0") +  (right_parent ? "1" : "0"), 2);
-    console.log(input);
+
+    return getRule(rule_number)[input];
 }
 
 function getColumnNumber(i) {
@@ -33,36 +36,42 @@ function getRightParent(i, data) { // wrap to last element of parent line if at 
 
 function getRule(ruleNumber) {
     var rule = [0,0,0,0,0,0,0,0];
-    var binaryRuleNumber = ruleNumber.toString(2);
-    for (var i = binaryRuleNumber.length - 1; i >= 0; i--) {
+    var binaryRuleNumber = ruleNumber.toString(2).split("").reverse();
+    for (var i = 0; i < binaryRuleNumber.length; i++) {
 	rule[i] = binaryRuleNumber[i] == '1' ? 255 : 0;
     }
-    return rule.reverse();
+    return rule;
 }
 
 
-var canvas = document.getElementById("foo");
+var canvas = Canvas.createCanvas(1024, 1024, "png");//document.getElementById("foo");
 var ctxt =  canvas.getContext("2d");
 
 var imageData = ctxt.getImageData(0, 0, ctxt.canvas.height, ctxt.canvas.width);
 
-console.log("width:" + ctxt.canvas.width);
-console.log("height:" + ctxt.canvas.height);
-console.log(imageData.data);
+
+//var ruleString = document.location.search;
+//ruleString = ruleString.substring(1, ruleString.length);
+//console.log(getRule(parseInt(ruleString)));
 
 var data = imageData.data;
 for (var i=0; i < data.length; i += 4) {
 		  var fill;
 		  if (i < ctxt.canvas.width * 4 -1) {
 			  fill = Math.floor(Math.random() * 2)  == 1;
+			  	   if (i == 512 * 4) {
+			      fill = 1;
+			  } else {
+			      fill = 0;
+			      }
 		  } else {
-		      console.log("POS: " + getColumnNumber(i));
+
 			  
 		      var left_parent = getLeftParent(i, data);
 		      var middle_parent = getMiddleParent(i, data);
 		      var right_parent = getRightParent(i, data);
-			  fill = applyRule(left_parent, middle_parent, right_parent);
-			 		  }
+		      fill = applyRule(left_parent, middle_parent, right_parent, 90);
+		  }
 
 		  data[i + 0] = fill ? 0 : 255;    // R value
 		  data[i + 1] = fill ? 0 : 255;  // G value
@@ -70,5 +79,9 @@ for (var i=0; i < data.length; i += 4) {
 		  data[i + 3] = 255;  // A value
 }
 
-imageData = new ImageData(data, ctxt.canvas.width, ctxt.canvas.height);
+imageData = Canvas.createImageData(data, ctxt.canvas.width, ctxt.canvas.height);
 ctxt.putImageData(imageData, 0, 0);
+
+
+var buf = canvas.toBuffer();
+fs.writeFileSync("test.png", buf);
